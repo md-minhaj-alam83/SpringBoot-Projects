ab6652cb8421904c690536b685f832fa896c0a8d
Md Minhaj Alam <94886118+md-minhaj-alam83@users.noreply.github.com>
2024-10-24 22:31:02 +0530

    Add files via upload
    Spring Boot InitilizrProject

---
+++ 1.SpringInitializrProject/src/main/java/in/ns/main/SpringInitializrProjectApplication.java
@@ -0,0 +1,13 @@
+package in.ns.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializrProjectApplication.class, args);
+	}
+
+}
---
+++ 1.SpringInitializrProject/src/test/java/in/ns/main/SpringInitializrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ns.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
