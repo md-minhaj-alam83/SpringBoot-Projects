fb535051232a9fe82975a0cb8102d9dd969c79c5
Md Minhaj Alam <94886118+md-minhaj-alam83@users.noreply.github.com>
2024-10-26 22:31:05 +0530

    Add files via upload
    spring initializer project

---
+++ SpringInitializrProject/src/main/java/in/ns/main/controllers/MyController.java
@@ -0,0 +1,18 @@
+package in.ns.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+	
+	@GetMapping("/")
+	@ResponseBody
+	public String GetMessage()
+	{
+		return "Welcome to NarrowIt Solution";
+	}
+	
+}
